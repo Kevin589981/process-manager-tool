dd732532cc51fe616fef1aa55fc556bab8649555
Kevin589981 <3627713050@qq.com>
2025-08-16 16:36:34 +0800

    修改

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_readme():
 
 setup(
     name="kiro-process-manager",
-    version="1.1.1",
+    version="1.1.2",
     author="Kiro Process Manager Contributors",
     author_email="kevin3627713@gmail.com",
     description="非阻塞后台进程管理工具，解决 Kiro IDE 中服务启动阻塞问题",
